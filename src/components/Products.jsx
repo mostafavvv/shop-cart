8a2952d375d4bf433e1ba22d3992ee87c93409a8
abc <abc@example.com>
2022-06-23 08:12:10 +0200

    cart-component

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -19,7 +19,7 @@ export default class Products extends Component {
                     <div>
                         { formatCarrency(product.price)}
                     </div>
-                    <button className='button primary'>
+                    <button onClick={()=> this.props.addToCart(product)} className='button primary'>
                         Add To cart
                     </button>
                 </div>
